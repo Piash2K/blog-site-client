cbcdea8221e124c544dfbdec1c7abdcb7e4b4185
Piash Islam <piashislam2k@gmail.com>
2026-02-04 16:35:50 +0600

    integrating shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <h1> Hello, Next.js!</h1>
+    <div>
+      <Button  variant="outline"> Click Here</Button>
     </div>
   );
 }
