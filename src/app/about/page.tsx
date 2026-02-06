445c8a88d1a5371b51e0e1dd5c60654046904fc9
Piash Islam <piashislam2k@gmail.com>
2026-02-06 21:35:45 +0600

    exploring the current implementation limitation for multiple layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,8 @@
-
+export const dynamic = 'force-dynamic';
 
 export default async function aboutPage() {
   await new Promise((resolve) => setTimeout(resolve, 4000));
-  throw new Error('Failed to load about page data');
+  // throw new Error('Failed to load about page data');
   return <div>This is about page component</div>;
 }
+  
